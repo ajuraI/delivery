0f525107c741fb98a8462e34cc488a4936eaaee7
ANCHUTIN Yury <Yury.ANCHUTIN@raiffeisen.ru>
2026-06-30 22:15:41 +0300

    add domain service

--- src/main/java/microarch/delivery/core/domain/services/OrderDistributionDomainService.java
+++ src/main/java/microarch/delivery/core/domain/services/OrderDistributionDomainService.java
@@ -1,12 +1,12 @@
 package microarch.delivery.core.domain.services;
 
-import libs.errs.UnitResult;
 import libs.errs.Error;
+import libs.errs.Result;
 import microarch.delivery.core.domain.model.courier.Courier;
 import microarch.delivery.core.domain.model.order.Order;
 
 import java.util.List;
 
 public interface OrderDistributionDomainService {
-    UnitResult<Error> distributeOrder(Order order, List<Courier> couriers);
+    Result<Courier, Error> distributeOrder(Order order, List<Courier> couriers);
 }
