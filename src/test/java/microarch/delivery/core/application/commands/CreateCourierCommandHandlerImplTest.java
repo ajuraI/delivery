d885472e1b54000a4180c8994182a816ad39c8ab
ANCHUTIN Yury <Yury.ANCHUTIN@raiffeisen.ru>
2026-07-05 18:02:37 +0300

    add controller

--- src/test/java/microarch/delivery/core/application/commands/CreateCourierCommandHandlerImplTest.java
+++ src/test/java/microarch/delivery/core/application/commands/CreateCourierCommandHandlerImplTest.java
@@ -1,12 +1,14 @@
 package microarch.delivery.core.application.commands;
 
 import libs.errs.Error;
-import libs.errs.UnitResult;
+import libs.errs.Result;
 import microarch.delivery.core.domain.model.courier.Courier;
 import microarch.delivery.core.domain.model.kernel.Location;
 import microarch.delivery.core.ports.CourierRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -19,13 +21,14 @@ class CreateCourierCommandHandlerImplTest {
         CourierRepository courierRepository = mock(CourierRepository.class);
         CreateCourierCommandHandler handler = new CreateCourierCommandHandlerImpl(courierRepository);
 
-        UnitResult<Error> result = handler.handle(new CreateCourierCommand("Ivan"));
+        Result<UUID, Error> result = handler.handle(new CreateCourierCommand("Ivan"));
 
         ArgumentCaptor<Courier> courierCaptor = ArgumentCaptor.forClass(Courier.class);
         verify(courierRepository).add(courierCaptor.capture());
         Courier courier = courierCaptor.getValue();
 
         assertThat(result.isSuccess()).isTrue();
+        assertThat(result.getValue()).isEqualTo(courier.getId());
         assertThat(courier.getName()).isEqualTo("Ivan");
         assertThat(courier.getLocation()).isEqualTo(new Location(1, 1));
     }
