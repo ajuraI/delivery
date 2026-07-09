6830ca043d5da63e1a696805bd85fb88b32072a6
ANCHUTIN Yury <Yury.ANCHUTIN@raiffeisen.ru>
2026-07-09 13:53:22 +0300

    add logic

--- src/main/java/microarch/delivery/adapters/in/kafka/BasketConfirmedIntegrationEventConsumer.java
+++ src/main/java/microarch/delivery/adapters/in/kafka/BasketConfirmedIntegrationEventConsumer.java
@@ -1,19 +1,57 @@
 package microarch.delivery.adapters.in.kafka;
 
-import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import libs.errs.Error;
+import libs.errs.UnitResult;
+import microarch.delivery.core.application.commands.CreateOrderCommand;
+import microarch.delivery.core.application.commands.CreateOrderCommandHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import queues.basket.events.BasketEventsProto;
 
+import java.util.UUID;
+
 @Component
 public class BasketConfirmedIntegrationEventConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(BasketConfirmedIntegrationEventConsumer.class);
+
+    private final CreateOrderCommandHandler createOrderCommandHandler;
+
+    public BasketConfirmedIntegrationEventConsumer(CreateOrderCommandHandler createOrderCommandHandler) {
+        this.createOrderCommandHandler = createOrderCommandHandler;
+    }
 
     @KafkaListener(
             topics = "${app.kafka.basket-events-topic}",
             groupId = "${spring.kafka.consumer.group-id}"
     )
-    public void listen(byte[] payload) throws InvalidProtocolBufferException {
-        BasketEventsProto.BasketConfirmedIntegrationEvent event =
-                BasketEventsProto.BasketConfirmedIntegrationEvent.parseFrom(payload);
-    }
+    public void listen(String message) {
+        try {
+            BasketEventsProto.BasketConfirmedIntegrationEvent.Builder builder =
+                    BasketEventsProto.BasketConfirmedIntegrationEvent.newBuilder();
+            JsonFormat.parser().merge(message, builder);
+
+            BasketEventsProto.BasketConfirmedIntegrationEvent event = builder.build();
+
+            UnitResult<Error> result = createOrderCommandHandler.handle(new CreateOrderCommand(
+                    UUID.fromString(event.getBasketId()),
+                    event.getAddress().getCountry(),
+                    event.getAddress().getCity(),
+                    event.getAddress().getStreet(),
+                    event.getAddress().getHouse(),
+                    event.getAddress().getApartment(),
+                    event.getVolume()
+            ));
+
+            if (result.isFailure()) {
+                log.error("Failed to create order from basket event: {}", result.getError().serialize());
+                return;
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    };
 }
